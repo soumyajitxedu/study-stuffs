d149caf1d558e25dbc4435c32962eae7aec31dee
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-08-03 17:16:30 +0530

    in and de creasing pattern.py

---
+++ in and de creasing pattern.py
@@ -0,0 +1,13 @@
+
+#in decending order
+n = 5 
+for i in range(n,0,-1):
+    for j in range(i):
+        print("x", end = " ")
+    print()
+
+y = 5 
+for q in range(1, y+1):
+    for h in range(1 , q+1):
+        print("^", end = " ")
+    print()
